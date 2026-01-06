b2240221a3785e7f9773037893804dd28624cdfb
Vijay Kumar <gvijay2403@gmail.com>
2026-01-06 19:13:41 +0530

    Set wrapper to scroll to top

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
+import { useLayoutEffect } from "react";
 import HomePage from "./pages/HomePage";
 import ContactsPage from "./pages/ContactsPage";
 import AllClubsPage from "./pages/AllClubsPage";
@@ -10,22 +11,32 @@ import FoodCourt from "./pages/FoodCourt";
 import Layout from "./components/Layout";
 const BASEURL = process.env.REACT_APP_BASEURL||"/welfare-board";
 
+const Wrapper = ({children}) => {
+  const location = useLocation();
+  useLayoutEffect(() => {
+    document.documentElement.scrollTo(0, 0);
+  }, [location.pathname]);
+  return children
+}
+
 function App() {
   return (
     <BrowserRouter basename={`${BASEURL}`} >
-      <Routes>
-        <Route element={<Layout />}> 
-          <Route index element={<HomePage />} />
-          <Route path="contacts" element={<ContactsPage />} />
-          <Route path="clubs" element={<AllClubsPage />} />
-          <Route path="club/:name" element={<EachClubPage />} />
-          <Route path="events" element={<EachEventPage />} />
-          <Route path="event/:id" element={<EachEventPage />} />
-          <Route path="resources" element={<SWB/>}/>
-          <Route path="counsellors" element={<Counsellors/>}/>
-          <Route path="foodcourt" element={<FoodCourt/>}/>
-        </Route>
-      </Routes>
+      <Wrapper>
+        <Routes>
+          <Route element={<Layout />}> 
+            <Route index element={<HomePage />} />
+            <Route path="contacts" element={<ContactsPage />} />
+            <Route path="clubs" element={<AllClubsPage />} />
+            <Route path="club/:name" element={<EachClubPage />} />
+            <Route path="events" element={<EachEventPage />} />
+            <Route path="event/:id" element={<EachEventPage />} />
+            <Route path="resources" element={<SWB/>}/>
+            <Route path="counsellors" element={<Counsellors/>}/>
+            <Route path="foodcourt" element={<FoodCourt/>}/>
+          </Route>
+        </Routes>
+      </Wrapper>
     </BrowserRouter>
   );
 }
